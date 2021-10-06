23e998d5dd625a767628f3c574f49aa16a6c5162
Ivo Edgar <ivo.edgy@gmail.com>
2021-10-06 19:13:15 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+let posX = randint(10, 118)
+let posY = randint(20, 54)
+let revX = Math.randomBoolean()
+let revY = Math.randomBoolean()
+basic.forever(function () {
+    kitronik_VIEW128x64.show("TESTE DE DISPLAY", 1, kitronik_VIEW128x64.ShowAlign.Centre)
+    kitronik_VIEW128x64.setPixel(posX, posY)
+    basic.pause(200)
+    kitronik_VIEW128x64.clearPixel(posX, posY)
+    if (revX) {
+        posX += -1
+    } else {
+        posX += 1
+    }
+    if (revY) {
+        posY += -1
+    } else {
+        posY += 1
+    }
+    if (posX < 10 || posX > 118) {
+        revX = !(revX)
+    }
+    if (posY < 20 || posY < 54) {
+        revY = !(revY)
+    }
+})
